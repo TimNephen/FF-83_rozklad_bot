796de56760d9ca8a75d69d65a2fc252c5ced2afc
TimNephen <55574250+TimNephen@users.noreply.github.com>
2020-09-22 14:13:50 +0300

    Update Rozklad_Bot.py

--- Rozklad_Bot.py
+++ Rozklad_Bot.py
@@ -14,7 +14,7 @@ def google():
     scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope )
     client = gspread.authorize(creds)
-    return client.open("Домашка").sheet1
+    return client.open("ДЗшки").sheet1
 
 #первые стартовые кнопки (далее расписаны что каждая кнопка делает)
 @bot.message_handler(commands=["rozklad", "start"])
